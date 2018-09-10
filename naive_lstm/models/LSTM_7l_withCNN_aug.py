d748b1217ac5b4f7e60eaaf5089da0494cbd250b
jinyu <zhaojinyu19@163.com>
2018-09-10 12:03:29 +0800

    succesful training on 8times augmentation data

--- naive_lstm/models/LSTM_7l_withCNN_aug.py
+++ naive_lstm/models/LSTM_7l_withCNN_aug.py
@@ -1,5 +1,4 @@
 
-
 # coding: utf-8
 
 # naive LSTM model trained with smooth data
@@ -12,20 +11,19 @@ import pickle
 import math
 import time
 import numpy as np
+import copy
 from torch.autograd import Variable
 
 device = torch.device(3 if torch.cuda.is_available() else "cpu")
     
 class DecoderRNN(nn.Module):
-    def __init__(self, input_size, augmented_size, hidden_size, output_size, batch_size = 1, batch_length = 100, dropout_p = 0):
+    def __init__(self, input_size, augmented_size, hidden_size, output_size, dropout_p = 0):
         super(DecoderRNN, self).__init__()
         self.input_size = input_size
         self.augmented_size = augmented_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.dropout_p = dropout_p
-        self.batch_size = batch_size
-        self.batch_length = batch_length
         self.verbose = (self.dropout_p != 0)
 
         self.cnn1 = nn.Conv1d(self.input_size, self.augmented_size//2 , kernel_size=3, padding=1)
@@ -51,6 +49,8 @@ class DecoderRNN(nn.Module):
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input):
+        self.batch_size = input.shape[0]
+        
         self.hidden1 = self.init_hidden()
         self.hidden2 = self.init_hidden()
         self.hidden3 = self.init_hidden()
@@ -94,7 +94,7 @@ class DecoderRNN(nn.Module):
             output = self.dropout6(output)
         output, self.hidden7 = self.lstm_7(output + output_5, self.hidden7)  # skip_connection 5
         
-        output = self.out(output).view(self.batch_size, self.batch_length,1)
+        output = self.out(output).view(self.batch_size, -1,1)
         # output = self.softmax(output)
         return output
 
@@ -103,22 +103,31 @@ class DecoderRNN(nn.Module):
                 torch.randn(2, 1, self.hidden_size // 2, device=device))
 print(torch.cuda.is_available())
 
+def pad(vector, pad, dim=0):
+    pad_size=list(vector.shape)
+    #print(pad_size)
+    pad_size[dim]=pad-vector.size(dim)
+    #print(pad_size[dim])
+    if pad_size[dim]<0:
+        print("FATAL ERROR: pad_size=100 not enough!")
+    return torch.cat([vector, torch.zeros(*pad_size).type(vector.type())], dim=dim)
 
-BATCH_SIZE = 40
+
+BATCH_SIZE = 30
 def validate(decoder, val_x, val_y, val_threshold = 0.5):
 
     count = 0
     total = 0
     total_1 = 0
     
-    val_set = data_utils.TensorDataset(val_x, val_y)
-    val_loader=data_utils.DataLoader(dataset=val_set, batch_size=BATCH_SIZE, drop_last=True, shuffle=True) 
+    #val_set = data_utils.TensorDataset(val_x, val_y)
+    #val_loader=data_utils.DataLoader(dataset=val_set, batch_size=BATCH_SIZE, drop_last=True, shuffle=True) 
 
     
-    for i, (val_x, val_y) in enumerate(val_loader):
-        X = val_x.to(device).float()
+    for i in range(len(val_x)):
+        X = torch.from_numpy(val_x[i]).to(device).float()
         result = decoder(X).squeeze().cpu().detach().numpy()
-        Y = val_y.squeeze().numpy()
+        Y = val_y[i].squeeze()
         gt = (Y == 1).astype(int)
         pr = (result > 0.5).astype(int)
         count += np.sum(gt * pr)
@@ -138,14 +147,14 @@ def penalty_loss(penalty, criterion, output, target):
     loss = 0
     batch_size = target.shape[0]
     for j in range(target.shape[0]):
-        for i in range(target.shape[0]):
+        for i in range(target.shape[1]):
             if int(target[j, i]) == 1:
                 loss += penalty[0] * criterion(output[j, i], target[j, i])
             else:
                 loss += penalty[1] * criterion(output[j, i], target[j, i])
     return loss/batch_size
 
-def train(input_tensor, target_tensor, decoder, decoder_optimizer, criterion, penalty = (1, 0.5)):
+def train(input_tensor, target_tensor, decoder, decoder_optimizer, criterion, penalty = (1, 0.6)):
     decoder_optimizer.zero_grad()
     
     loss = 0
@@ -166,59 +175,52 @@ def train(input_tensor, target_tensor, decoder, decoder_optimizer, criterion, pe
 
     return loss.item()
 
-def pad(vector, pad, dim=0):
-    pad_size=list(vector.shape)
-    #print(pad_size)
-    pad_size[dim]=pad-vector.size(dim)
-    #print(pad_size[dim])
-    if pad_size[dim]<0:
-        print("FATAL ERROR: pad_size=100 not enough!")
-    return torch.cat([vector, torch.zeros(*pad_size).type(vector.type())], dim=dim)
-
-def factorize(data_X, data_Y, flag, batch_size, batch_length):
-    if flag:
-        new_X = []
-        new_Y = []
-        for i in range(len(data_X)):
-            X, Y = data_X[i], data_Y[i]
-            flag = []
-            for loc, j in enumerate(Y):
-                if j == 1:
-                    flag.append(loc)
-            prev = 0
-            for j in range(4, len(flag), 5):
-                new_X.append(pad(torch.from_numpy(X[prev:flag[j]]), batch_length))
-                new_Y.append(pad(torch.from_numpy(Y[prev:flag[j]]), batch_length))
-                prev = flag[j]
-        batch_X = []
-        batch_Y = []
-        """for i in range(0, len(new_X), batch_size):
-            if (i + batch_size) > (len(new_X) - 1):
-                break
-            batch_x = []
-            batch_y = []
-            for j in range(batch_size):
-                batch_x.append(pad(new_X[i + j], batch_length))
-                batch_y.append(pad(new_Y[i + j], batch_length))
-            batch_X.append(torch.stack(batch_x))
-            batch_Y.append(torch.stack(batch_y))
-        return batch_X, batch_Y"""
-        return torch.stack(new_X), torch.stack(new_Y)
-    
+def factorize(data_X, data_Y, size, batch_size, batch_length):
+    new_X = []
+    new_Y = []
+    for i in range(len(data_X)):
+        X, Y = data_X[i], data_Y[i]
+        flag = []
+        for loc, j in enumerate(Y):
+            if j == 1:
+                flag.append(loc)
+        prev = 0
+        for j in range(4, len(flag), 10):
+            new_X.append(pad(torch.from_numpy(X[prev:flag[j]]), batch_length))
+            new_Y.append(pad(torch.from_numpy(Y[prev:flag[j]]), batch_length))
+            prev = flag[j]
+    batch_X = []
+    batch_Y = []
+    """for i in range(0, len(new_X), batch_size):
+        if (i + batch_size) > (len(new_X) - 1):
+            break
+        batch_x = []
+        batch_y = []
+        for j in range(batch_size):
+            batch_x.append(pad(new_X[i + j], batch_length))
+            batch_y.append(pad(new_Y[i + j], batch_length))
+        batch_X.append(torch.stack(batch_x))
+        batch_Y.append(torch.stack(batch_y))
+    return batch_X, batch_Y"""
+    print(torch.stack(new_X).shape, torch.stack(new_Y).shape)
+    return torch.stack(new_X), torch.stack(new_Y)
     
 import torch.utils.data as data_utils
 class CrossValidator:
-    def __init__(self, model, partition=5, decoder=None, batch_size=BATCH_SIZE, batch_length = 100, epochs=10, lr=1e-2, 
-                 augment_data=True, print_every = 1000, plot_every = 100, gamma = 0.1, mode="crossvalidation"):
+    def __init__(self, model, partition=5, decoder=None, batch_size=BATCH_SIZE, batch_length = 200, epochs=10, lr=1e-2, 
+                 augment_data=0, print_every = 1000, plot_every = 100, gamma = 0.1, penalty = [1,0.5]):
         self.model=model
         self.data_X = []
         self.data_Y = []
-        with open("/home/yiqin/2018summer_project/data/smooth_3d1s_augmented2.pkl", "rb") as f:
+        self.augment_data_size=augment_data
+        with open("/home/yiqin/2018summer_project/data/smooth_3d1s_augmented.pkl", "rb") as f:
             data= pickle.load(f)
             for i in range(len(data)):
                 self.data_X.append(data[i][0])
                 self.data_Y.append(data[i][1])
-        self.data_size = len(data)
+            
+        
+        self.data_size = len(self.data_X)
         self.partition=partition
         self.decoder=decoder
         self.train_X=[]
@@ -233,11 +235,10 @@ class CrossValidator:
         self.batch_length=batch_length
         self.epochs=epochs
         self.lr=lr
-        self.augment_data_flag=augment_data
         self.gamma = gamma
         self.print_every = print_every
         self.plot_every = plot_every
-        self.mode = mode
+        self.penalty = penalty
         
         
     def create_data(self, part):
@@ -253,6 +254,32 @@ class CrossValidator:
             else:
                 val_X.append(np.array(self.data_X[i]))
                 val_Y.append(np.array(self.data_Y[i]))
+                
+        new_X = []
+        for loc, item in enumerate(train_X):
+            x = copy.deepcopy(item)
+            x[:,0] = x[:,0] + 0.5
+            new_X.append(x)
+        train_X += new_X
+        train_Y += train_Y
+        augmented_X = []
+        augmented_Y = []
+        augmented_X += train_X
+        augmented_Y += train_Y
+        for i in range(self.augment_data_size):
+            new_X = []
+            print(i)
+            if i == (self.augment_data_size //2):
+                continue
+            for loc, item in enumerate(train_X):
+                x = copy.deepcopy(item)
+                x[:,2] = x[:,2] + i - (self.augment_data_size // 2)
+                new_X.append(x)
+            augmented_X += new_X
+            augmented_Y += train_Y
+
+        train_Y = augmented_Y
+        train_X = augmented_X
         return train_X, train_Y, val_X, val_Y
     
     def tensorize(self, p):
@@ -274,25 +301,31 @@ class CrossValidator:
         rs = es - s
         return '%s (- %s)' % (self.asMinutes(s), self.asMinutes(rs))
 
+    def penalty_decay(self, penalty, epoch, decay):
+        penalty[1] = (1+decay)**epoch * penalty[1]
+        return penalty
+    
     def compute(self):
         for i in range(self.partition):
-            if self.mode == "training" and i > 0:
-                break
             start = time.time()
             temptrain_X, temptrain_Y, tempval_X, tempval_Y = self.create_data(i)
-            self.train_X, self.train_Y = factorize(temptrain_X, temptrain_Y, self.augment_data_flag, self.batch_size, self.batch_length)
-            self.val_X, self.val_Y = factorize(tempval_X, tempval_Y, self.augment_data_flag, self.batch_size, self.batch_length)
+            print(len(temptrain_X), len(temptrain_Y), len(tempval_X), len(tempval_Y))
+            self.train_X, self.train_Y = factorize(temptrain_X, temptrain_Y, self.augment_data_size, self.batch_size, self.batch_length)
+            #self.val_X, self.val_Y = factorize(tempval_X, tempval_Y, self.augment_data_size, self.batch_size, self.batch_length)
+            self.val_X, self.val_Y = tempval_X, tempval_Y
             train_set = data_utils.TensorDataset(self.train_X, self.train_Y)
             train_loader=data_utils.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, drop_last=True, shuffle=True) 
-
+            print(len(train_loader))
             
             print(i, "phase 1 completed.")
             
             cur_model = deepcopy(self.model).to(device)
             
-            optimizer = optim.SGD(cur_model.parameters(), lr = self.lr, weight_decay = 1e-5)
+            optimizer = optim.SGD(cur_model.parameters(), lr = self.lr)
     
             criterion = nn.SmoothL1Loss()
+        
+            penalty = self.penalty
 
             scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = self.gamma)
 
@@ -301,68 +334,68 @@ class CrossValidator:
             
             
             for j in range(self.epochs):
+                penalty = self.penalty_decay(penalty, j, decay = 0.05)
                 for num, (train_X, train_Y)in enumerate(train_loader):
                     input_tensor = train_X.to(device).float()
                     target_tensor = train_Y.to(device).float()
-                    #print(input_tensor.shape)
-                    loss = train(input_tensor, target_tensor, cur_model, decoder_optimizer=optimizer, criterion= criterion)
-                    #print(num, loss)
+                    loss = train(input_tensor, target_tensor, cur_model, decoder_optimizer=optimizer, criterion= criterion, penalty = penalty)
+                    #print(loss)
                     print_loss_total += loss
                     plot_loss_total += loss
                     
-                    if (num+1)%self.plot_every == 0:
+                    if num%self.plot_every == 0:
                         plot_loss_avg = plot_loss_total / self.plot_every
                         plot_loss_total = 0
                         self.loss_history.append(plot_loss_avg)
 
-                    if (num+1) % self.print_every == 0:
-                        
+                    if (num + 1) % self.print_every == 0:
                         print_loss_avg = print_loss_total / self.print_every
                         print_loss_total = 0
                         print("epoch %i"%j)
-                        if self.mode == "training":
-                            acc, one_acc, score = validate(cur_model, self.val_X, self.val_Y)
-                            self.precision_history.append(acc[:-1])
-                            self.recall_history.append(one_acc[:-1])
-                            print("validation accuracy:", acc)
-                            print("validation prediction accuracy:", one_acc)
                         p = self.timeSince(start, (num+j*len(train_loader)) / (self.epochs * len(train_loader)))
-                        print('%s (%d %d%%) %.4f' % (p, num + 1, (num + 1) / (self.epochs * len(train_loader)) * self.print_every, print_loss_avg))
-                        
-                        """if(score > self.best_acc):
+                        print('%s (%d %d%%) %.4f' % (p, num + 1, (num + 1) / (self.epochs * len(train_loader)) * self.print_every,
+                                                     print_loss_avg))
+                    if (num+1) % 1000 == 0:
+                        acc, one_acc, score = validate(cur_model, self.val_X, self.val_Y)
+                        self.precision_history.append(acc[:-1])
+                        self.recall_history.append(one_acc[:-1])
+                        print("validation accuracy:", acc)
+                        print("validation prediction accuracy:", one_acc)
+                        if(score > self.best_acc):
                         #    torch.save(cur_model.state_dict(), '/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN_best(cv).pt')
                             self.best_acc = score
-                        print("best_score:", self.best_acc)"""
-                if self.mode == "crossvalidation":        
-                    acc, one_acc, score = validate(cur_model, self.val_X, self.val_Y)
-                    self.precision_history.append(acc[:-1])
-                    self.recall_history.append(one_acc[:-1])
-                    print("validation accuracy:", acc)
-                    print("validation prediction accuracy:", one_acc)
+                        print("best_score:", self.best_acc)
+                        
+                        
+                        dic = {}
+                        dic["recall"] = self.precision_history
+                        dic["precision"] = self.recall_history
+                        dic["loss"] = self.loss_history
+                        f = open("/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN_batch_losses(cv)1.pkl", "wb")
+                        pickle.dump(dic, f)
 
                 scheduler.step()
-                if self.mode == "training":
-                    torch.save(cur_model.state_dict(), '/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN_aug(cv){}.pt'.format(j))
+                #torch.save(cur_model.state_dict(), '/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN(cv){}-{}.pt'.format(i,j))
                 
         return self.loss_history, self.precision_history, self.recall_history
+
 
 
 input_size = 3
 augmented_size = 32
 hidden_size = 256
 output_size = 1
-batch_size = 40
+batch_size = 30
 batch_length = 100
-model = DecoderRNN(input_size, augmented_size, hidden_size, output_size, batch_size, batch_length, dropout_p = 0).to(device)
-cv = CrossValidator(model, partition=13, epochs=8, batch_size = batch_size, print_every = 100, mode = "training")
+model = DecoderRNN(input_size, augmented_size, hidden_size, output_size, dropout_p = 0).to(device)
+cv = CrossValidator(model, partition=5, epochs=5, batch_size = batch_size, augment_data=11, print_every = 100, penalty = [1, 0.6])
 losses, precision, recall = cv.compute()
 
 
 dic = {}
 dic["loss"] = losses
-dic["precision"] = precision
-dic["recall"] = recall
+dic["precision"] = recall
+dic["recall"] = precision
 
-f = open("/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN_batch_losses(cv-l2).pkl", "wb")
+f = open("/home/yiqin/2018summer_project/saved_model/Bi-LSTM-CNN_batch_losses(cv)1.pkl", "wb")
 pickle.dump(dic, f)
-
